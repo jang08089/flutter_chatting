cf986c08017e39fd96d32866e1cf94960b8d95dd
장준혁 <3011716@gmail.com>
2025-11-13 20:51:42 +0900

    good

--- lib/data/model/profile.dart
+++ lib/data/model/profile.dart
@@ -9,13 +9,16 @@ abstract class Profile with _$Profile {
     required String nickname,
     @JsonKey(name: 'is_male') required bool isMale,
     required String sport,
-    required String location,
-    @JsonKey(name: 'created_at') required DateTime createdAt
-    
+    @JsonKey(name: 'full_nm') required String fullNm,
+    @JsonKey(name: 'emd_cd') required String emdCd,
+    @JsonKey(name: 'created_at', fromJson: _convertToDateTime)
+    required DateTime createdAt,
   }) = _Profile;
 
-  factory Profile.fromJson(Map<String, Object?> json) => _$ProfileFromJson(json);
+  factory Profile.fromJson(Map<String, Object?> json) =>
+      _$ProfileFromJson(json);
 }
-DateTime _convertToDateTime(String timeString){
+
+DateTime _convertToDateTime(String timeString) {
   return DateTime.parse(timeString);
 }
--- lib/data/model/profile.g.dart
+++ lib/data/model/profile.g.dart
@@ -10,14 +10,16 @@ _Profile _$ProfileFromJson(Map<String, dynamic> json) => _Profile(
   nickname: json['nickname'] as String,
   isMale: json['is_male'] as bool,
   sport: json['sport'] as String,
-  location: json['location'] as String,
-  createdAt: DateTime.parse(json['created_at'] as String),
+  fullNm: json['full_nm'] as String,
+  emdCd: json['emd_cd'] as String,
+  createdAt: _convertToDateTime(json['created_at'] as String),
 );
 
 Map<String, dynamic> _$ProfileToJson(_Profile instance) => <String, dynamic>{
   'nickname': instance.nickname,
   'is_male': instance.isMale,
   'sport': instance.sport,
-  'location': instance.location,
+  'full_nm': instance.fullNm,
+  'emd_cd': instance.emdCd,
   'created_at': instance.createdAt.toIso8601String(),
 };
